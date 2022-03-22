9bb7247ea532ac375fec4a993ee7569bef51cd2c
Min Jae Kim <kmj6832596@naver.com>
2022-03-22 23:02:17 +0900

    Add files via upload

---
+++ 10진수에서 2진수로 변환.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void Trantwo(int ten)
+{
+    int div = 0;
+    int na = 0;
+    int arr[100];
+    int count=1;
+    arr[0] = ten%2;
+    
+    while(ten>0)
+    {
+        ten = ten/2;
+        na =  ten%2;
+        arr[count] = na;
+        
+        if(ten<1)
+        {
+            break;
+        }
+        
+        printf("count:%d , na: %d , ten: %d \n",count,na,ten);
+        count++;
+    }
+    
+    
+    printf("%d\n",count);
+    printf("===============================\n");
+    printf("\n");
+    printf("\n"); 
+    for(int i=count-1; i>=0; i--)
+    {
+        printf("%d",arr[i]);
+    }
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("===============================\n");
+}
+
+
+
+int main()
+{
+   int ten,two,sixteen;
+    printf("10진수를 입력해주세요!: ");
+    scanf("%d",&ten);
+    Trantwo(ten);
+}
