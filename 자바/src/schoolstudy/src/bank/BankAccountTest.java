85391f8f71cfaf743a9d8989d8bb3126e04819f2
Min Jae Kim <kmj6832596@naver.com>
2022-04-04 10:27:31 +0900

    Add files via upload

---
+++ 자바/src/schoolstudy/src/bank/BankAccountTest.java
@@ -0,0 +1,19 @@
+package bank;
+
+public class BankAccountTest 
+{
+	public static void main(String[] args)
+	{
+		BankAccount Kim = new BankAccount("±ËπŒ¿Á","000729"); 
+		BankAccount Lee = new BankAccount("¿Âµ∂¥Î", "000123");
+		
+		Kim.Deposite(100000);
+		Lee.Deposite(16000);
+		
+		Kim.Withdraw(15800);
+		Lee.Withdraw(10000);
+		
+		Kim.ShowInformation();
+		Lee.ShowInformation();
+	}
+}
